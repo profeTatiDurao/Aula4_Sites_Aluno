2c5c0c047108bd782b28c989b83dada6aee81ad5
profeTatiDurao <biancaduraofernandes@gmail.com>
2025-09-08 14:02:17 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+ const emojis = ["🐶", "🐱", "🐰", "🦊", "🐸", "🐵", "🦁", "🐼"];
+    let cartas = [];
+    let primeira = null;
+    let segunda = null;
+    let bloqueado = false;
+    let movimentos = 0;
+
+    function embaralhar(array) {
+     
+
+    }
+
+    function criarCarta(emoji) {
+     
+    }
+
+    function virarCarta(carta) {
+      
+    }
+
+    function checarVitoria() {
+    
+    }
+
+    function iniciarJogo() {
+     
+    }
+
+    iniciarJogo();
